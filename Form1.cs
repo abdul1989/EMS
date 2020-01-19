5c97afb2cb5f7db5f3e0b539d8aa9bdac9cff1bb
ABDUL MATEEN <ABDUL MATEEN@DESKTOP-5NGMVDR>
2020-01-19 12:30:55 +0530

    added two buttons

--- Form1.cs
+++ Form1.cs
@@ -50,5 +50,10 @@ namespace EMS
         {
             MessageBox.Show("Hi");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("new btn");
+        }
     }
 }
